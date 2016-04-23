baa1d169be43947e45670b68560c3074bf560185
Kevin Li <kevinli120@yahoo.com>
2016-04-23 14:09:01 -0700

    first commit

---
+++ reply.js
@@ -0,0 +1,23 @@
+// setTimeout(reply, 5000);
+
+function reply() {
+    var chatBox = document.getElementsByName("message_body")[0];
+    var replyBut = document.getElementById("u_0_y");
+
+    var test = setInterval(function() {
+        chatBox.classList.remove("DOMControl_placeholder");
+        chatBox.value = "Same";
+
+        getEventListeners(chatBox).paste[0].listener();
+
+    }, 1000);
+
+    var replyClicker = setInterval(function() {
+        replyBut.click();
+    }, 2000);
+
+    var stopKittens = function() {
+        clearInterval(kitInt);
+        clearInterval(replyClicker);
+    };
+}
